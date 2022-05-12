a93569d6dceca13abd377e06742a6938a84c8843
amjennings <98051208+amjennings@users.noreply.github.com>
2022-05-11 21:25:14 -0500

    Add files via upload

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -11,3 +11,15 @@ total_summary <- summarize(suspension_table, mean = mean(PSI), median = median(P
 lot <- group_by(suspension_table, Manufacturing_Lot) 
 
 lot_summary <- summarize(lot, mean = mean(PSI), median = median(PSI), var = var(PSI), sd = sd(PSI))
+
+t.test(suspension_table$PSI, mu=1500)
+
+lot1 <- subset(suspension_table, Manufacturing_Lot=='Lot1')
+t.test(lot1$PSI, mu=1500)
+
+lot2 <- subset(suspension_table, Manufacturing_Lot=='Lot2')
+t.test(lot2$PSI, mu=1500)
+
+lot3 <- subset(suspension_table, Manufacturing_Lot=='Lot3')
+t.test(lot3$PSI, mu=1500)
+
